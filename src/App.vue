98236574b1df7f68fd114ffbd583364ad72490e6
LionelSebastian <lionel.sebastian.cifo@gmail.com>
2022-12-06 12:41:29 +0100

    Initial Commit => Vue app' created

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <header>
+ 
+  </header>
+
+  <main>
+
+  </main>
+</template>
+
+<style scoped>
+
+</style>
